3a761732ad726a96551472ab4695370cd9ba614b
Josh Lee <JoshCliffLee@gmail.com>
2024-01-11 14:10:34 +0000

    added post_detail view

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-haloegen-django-blog-vebalvl4yp.us2.codeanyapp.com',
  '.herokuapp.com', '8000-haloegen-djangoblog-xvzvie1sew3.ws-eu107.gitpod.io']
