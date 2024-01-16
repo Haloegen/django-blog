49c2250e4b20de9a7bfab446d2394b403bb18fb5
Josh Lee <JoshCliffLee@gmail.com>
2024-01-16 11:33:53 +0000

    deploying to heroku, debug to false

--- about/admin.py
+++ about/admin.py
@@ -3,5 +3,6 @@ from .models import About
 from django_summernote.admin import SummernoteModelAdmin
 # Register your models here.
 
+@admin.register(About)
 class AboutAdmin(SummernoteModelAdmin):
-    summernote_fields = ('content')
+    summernote_fields = ('content',)
--- about/views.py
+++ about/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.views import generic
 from .models import About
 
 # Create your views here.
-def about_me(request, title):
-    About.objects.all().order_by('-updated_on').first()
+def about_me(request):
+    about = About.objects.all().order_by('-updated_on').first()
 
-    return render(request, "about/about.html", {"about": about,})
+    return render(request, "about/about.html", {"about": about})
